762191b01006ef3bc54b77cf63cf443d19d812b1
Alexey Panfilkin <a.panfilkin@oblakogroup.ru>
2018-03-15 08:22:44 +0300

    Add json loader

--- module-app/modules/random_generator/random_generator.js
+++ module-app/modules/random_generator/random_generator.js
@@ -1,9 +1,21 @@
 var RandGenerator = function(){
-
-	this.randomInt = function(min, max){
+	this.model_path = './modules/random_generator/model.json'
+	this.model = {};
+	this.randomInt = (min, max) => {
 		var rand = min + Math.random() * (max - min)
 		rand = Math.round(rand);
 		return rand;
 	}
 
+	this.load_model = () => {
+		$.get(this.model_path, (res)=>{
+			console.log(res)
+			this.model = res;
+		})
+	}
+
+	this.init = () => {
+		this.load_model();
+	}
+	this.init();
 } 
